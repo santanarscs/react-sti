736111ed4a5e2fe87045dd7c06e884e081b94e37
santanarscs <raphaelstn@gmail.com>
2020-10-07 14:35:45 -0300

    feat: configure theme app

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 import GlobalStyles from './styles/global';
 import Routes from './routes';
 import AppProvider from './hooks';
 import { Container } from './styles/components';
+import { dark } from './styles/themes';
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
-      <AppProvider>
-        <Container>
-          <Routes />
-        </Container>
-        <GlobalStyles />
-      </AppProvider>
-    </BrowserRouter>
+    <ThemeProvider theme={dark}>
+      <BrowserRouter>
+        <AppProvider>
+          <Container>
+            <Routes />
+          </Container>
+          <GlobalStyles />
+        </AppProvider>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 };
 

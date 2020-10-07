edca254260f167c7d93cdfd2782c91bdb7d15592
santanarscs <raphaelstn@gmail.com>
2020-10-07 17:52:03 -0300

    chore: clean components

--- src/pages/_layouts/Default/styles.ts
+++ src/pages/_layouts/Default/styles.ts
@@ -3,18 +3,12 @@ import styled from 'styled-components';
 export const Container = styled.div`
   flex: 1;
   display: flex;
-  @media (max-width: 900px) {
-    flex-direction: column;
-  }
 `;
 
 export const Content = styled.div`
   width: 100%;
-  margin-left: 200px;
+  margin-left: 10rem;
   flex: 1;
   display: flex;
   flex-direction: column;
-  @media (max-width: 900px) {
-    margin: 40px auto;
-  }
 `;

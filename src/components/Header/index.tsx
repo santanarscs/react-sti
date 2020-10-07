1d14a592d29663fde3cdd7255f6c22c35b3a281a
santanarscs <raphaelstn@gmail.com>
2020-10-07 15:19:18 -0300

    feat: remove button header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,22 +1,10 @@
 import React from 'react';
-import { FiPower } from 'react-icons/fi';
-import { useAuth } from '../../hooks/auth';
 import { Container, Content } from './styles';
 
 const Header: React.FC = () => {
-  const { signOut } = useAuth();
-
-  function handleSignOut(): void {
-    signOut();
-  }
   return (
     <Container>
-      <Content>
-        <button type="button" onClick={handleSignOut}>
-          <FiPower size={25} />
-          Sair
-        </button>
-      </Content>
+      <Content />
     </Container>
   );
 };

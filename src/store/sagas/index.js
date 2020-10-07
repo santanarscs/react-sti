f369d909aa429bb403b849ded4ca9587ab0a2567
santanarscs <raphaelstn@gmail.com>
2020-10-07 14:27:39 -0300

    feat: clean project

--- src/store/sagas/index.js
+++
@@ -1,8 +0,0 @@
-import { all, takeLatest } from 'redux-saga/effects';
-
-import { getOrders } from './orders';
-import { OrdersTypes } from '../ducks/orders';
-
-export default function* rootSaga() {
-	return yield all([takeLatest(OrdersTypes.GET_ORDERS_REQUEST, getOrders)]);
-}
--- src/store/sagas/orders.js
+++
@@ -1,8 +0,0 @@
-import { call, put } from 'redux-saga/effects';
-import api from '../../services/api';
-import OrdersActions from '../ducks/projects';
-
-export function* getOrders() {
-	const response = yield call(api.get, '/orders');
-	yield put(OrdersActions.getOrdersSuccess(response.data));
-}

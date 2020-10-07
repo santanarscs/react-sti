c864abdcc345c48790722bb76ed7a6b53c1b64c7
santanarscs <raphaelstn@gmail.com>
2020-10-07 15:19:53 -0300

    feat: remove imports deprecateds

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,17 +5,13 @@ import Route from './Route';
 
 import SignIn from '../pages/SignIn';
 import Dashboard from '../pages/Dashboard';
-import Documents from '../pages/Documents';
 
-import Managers from '../pages/Manager';
 import Users from '../pages/Users';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={SignIn} />
     <Route path="/dashboard" isPrivate component={Dashboard} />
-    <Route path="/documents" isPrivate component={Documents} />
-    <Route path="/managers" isPrivate component={Managers} />
     <Route path="/users" isPrivate component={Users} />
   </Switch>
 );

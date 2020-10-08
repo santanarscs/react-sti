87f10bf3aa5a031b716f26f49c8a3813e1fef68b
santanarscs <raphaelstn@gmail.com>
2020-10-08 17:42:32 -0300

    style: update style input component

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -4,13 +4,13 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   input {
-    height: 4rem;
+    height: 5rem;
     padding: 1rem;
-    border-radius: 0.3rem;
+    border-radius: ${({ theme }) => theme.radii.default};
     border: 0.1rem solid rgba(0, 0, 0, 0.3);
     background-color: rgba(0, 0, 0, 0.1);
-    color: #f6f6f6;
-    margin-top: 0.8rem;
+    color: #ccd1dd;
+    margin-top: 1rem;
     transition: border 0.15s ease;
     font-size: 1.6rem;
     &:focus {

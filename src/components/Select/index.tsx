010d7ae859a2e6e70beee3744f0d9a867f9f4579
santanarscs <raphaelstn@gmail.com>
2020-10-09 10:11:00 -0300

    feat: implement ref in select component

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -1,30 +1,95 @@
-import React, { SelectHTMLAttributes } from 'react';
+import React, { useRef, useEffect } from 'react';
+import ReactSelect, {
+  OptionTypeBase,
+  Props as SelectProps,
+  StylesConfig,
+  Theme,
+} from 'react-select';
+import { useField } from '@unform/core';
 import { Container } from './styles';
 
-interface IOptionValue {
-  value: string | number;
+export interface IOptionValue {
+  value: string;
   label: string;
 }
 
-interface IInputProps extends SelectHTMLAttributes<HTMLSelectElement> {
+interface IProps extends SelectProps<OptionTypeBase> {
   name: string;
-  label?: string;
-  options: IOptionValue[];
+  label: string;
 }
 
-const Select: React.FC<IInputProps> = ({ label, name, options, ...rest }) => {
+const SelectInput: React.FC<IProps> = ({ name, label, ...rest }) => {
+  const selectRef = useRef(null);
+  const { fieldName, defaultValue, registerField } = useField(name);
+
+  const colourStyles: StylesConfig = {
+    control: (styles) => ({
+      ...styles,
+      borderRadius: 8,
+      borderColor: '#1F232C',
+      fontSize: 16,
+      height: 45,
+    }),
+    option: (styels) => ({
+      ...styels,
+      color: '#F4EDE8',
+    }),
+  };
+
+  const themeProps = (theme: Theme): Theme => {
+    return {
+      ...theme,
+      colors: {
+        ...theme.colors,
+        primary: '#386ae8',
+        neutral0: '#2C323E',
+        primary25: '#666360',
+        primary50: '#999591',
+        neutral80: '#F4EDE8',
+        neutral30: '#386ae8',
+      },
+    };
+  };
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: selectRef.current,
+      getValue: (ref: any) => {
+        if (rest.isMulti) {
+          if (!ref.state.value) {
+            return [];
+          }
+          return ref.state.value.map((option: IOptionValue) => option.value);
+        }
+        if (!ref.state.value) {
+          return '';
+        }
+        return ref.state.value.value;
+      },
+      setValue: (ref: ReactSelect, value: IOptionValue) => {
+        ref.select.selectOption(value);
+      },
+      clearValue: (ref: ReactSelect) => {
+        ref.select.clearValue();
+      },
+    });
+  }, [fieldName, registerField, rest.isMulti]);
+
   return (
     <Container>
-      {label && <label htmlFor={name}>{label}</label>}
-      <select name={name} {...rest}>
-        <option value="null">Selecione...</option>
-        {options.map((option) => (
-          <option key={option.value} value={option.value}>
-            {option.label}
-          </option>
-        ))}
-      </select>
+      <label htmlFor={name}>{label}</label>
+      <ReactSelect
+        defaultValue={defaultValue}
+        ref={selectRef}
+        classNamePrefix="react-select"
+        styles={colourStyles}
+        theme={themeProps}
+        maxMenuHeight={250}
+        {...rest}
+      />
     </Container>
   );
 };
-export default Select;
+
+export default SelectInput;
--- src/components/Select/styles.ts
+++ src/components/Select/styles.ts
@@ -3,25 +3,8 @@ import styled from 'styled-components';
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
-  flex: 1;
-  margin-bottom: 15px;
-
-  select {
-    height: 40px;
-    padding: 10px;
-    border-radius: 3px;
-    border: 1px solid rgba(0, 0, 0, 0.3);
-    background-color: rgba(0, 0, 0, 0.1);
-    color: #f6f6f6;
-    margin-top: 8px;
-    transition: border 0.15s ease;
-    font-size: 16px;
-    &:focus {
-      border-color: #7289da;
-    }
-    option {
-      border: 1px solid rgba(0, 0, 0, 0.3);
-      background-color: #333;
-    }
+  label {
+    margin-bottom: 1rem;
   }
+  margin-bottom: 1.5rem;
 `;

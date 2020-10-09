77d4eb3dccd53215e42b0140d292f7265265ce6a
Raphael Santana <raphaelstn@gmail.com>
2020-10-09 15:56:48 -0300

    chore: define user interface

---
+++ src/interfaces/IUser.ts
@@ -0,0 +1,16 @@
+export default interface IUser {
+  id: string;
+  name: string;
+  full_name: string;
+  graduation: string;
+  specialty: string;
+  saram: string;
+  mail: string;
+  section: string;
+  situation: string;
+  phone: string;
+  birthday: string;
+  last_promotion: string;
+  sequence: string;
+  image_url?: string;
+}

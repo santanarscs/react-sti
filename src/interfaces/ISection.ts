5e2f606b87958c1543c7e27e63d198db86fd2f2c
santanarscs <raphaelstn@gmail.com>
2020-10-13 14:32:44 -0300

    chore: define interfaces for user

---
+++ src/interfaces/ISection.ts
@@ -0,0 +1,6 @@
+export default interface ISection {
+  name: string;
+  description: string;
+  created_at: string;
+  updated_at: string;
+}
--- src/interfaces/IUser.ts
+++ src/interfaces/IUser.ts
@@ -1,16 +1,25 @@
+import IGraduation from './IGraduation';
+import ISpecialty from './ISpecialty';
+import IBoard from './IBoard';
+import ISection from './ISection';
+
 export default interface IUser {
   id: string;
+  _id: string;
   name: string;
-  full_name: string;
-  graduation: string;
-  specialty: string;
+  email: string;
+  password: string;
+  specialty: ISpecialty;
+  board: IBoard;
+  graduation: IGraduation;
+  section: ISection;
   saram: string;
-  mail: string;
-  section: string;
+  full_name: string;
   situation: string;
   phone: string;
-  birthday: string;
-  last_promotion: string;
+  birthday: Date;
+  last_promotion: Date;
   sequence: string;
-  image_url?: string;
+  provider: boolean;
+  avatar?: string;
 }

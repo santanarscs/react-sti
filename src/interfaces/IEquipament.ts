2af8a14d309de197f2e6a6a71ad632425da50b76
santanarscs <raphaelstn@gmail.com>
2020-10-14 16:19:38 -0300

    chore(equipaments): update interface

--- src/interfaces/IEquipament.ts
+++ src/interfaces/IEquipament.ts
@@ -1,5 +1,21 @@
+interface IMovimentation {
+  id: string;
+  date: Date;
+  user?: {
+    name: string;
+    graduation: {
+      name: string;
+    };
+  };
+  section: {
+    name: string;
+  };
+}
+
 export default interface IEquipament {
   id: string;
   description: string;
   bpm: string;
+  service_tag: string;
+  movimentations?: IMovimentation[];
 }

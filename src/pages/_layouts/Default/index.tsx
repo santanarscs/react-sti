261c4fe4d2f293cba6e2dadb10b2d49b96c208d4
santanarscs <raphaelstn@gmail.com>
2020-10-16 10:25:24 -0300

    chore: remove header component default layout

--- src/pages/_layouts/Default/index.tsx
+++ src/pages/_layouts/Default/index.tsx
@@ -2,16 +2,12 @@ import React from 'react';
 
 import { Container, Content } from './styles';
 import Sidebar from '../../../components/Sidebar';
-import Header from '../../../components/Header';
 
 const Default: React.FC = ({ children }): JSX.Element => {
   return (
     <Container>
       <Sidebar />
-      <Content>
-        <Header />
-        {children}
-      </Content>
+      <Content>{children}</Content>
     </Container>
   );
 };

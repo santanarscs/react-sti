509217501ada9f8f54dc1d222547cc15d760baee
santanarscs <raphaelstn@gmail.com>
2020-10-16 10:25:58 -0300

    feat(equipaments): implement search in list equipament

--- src/pages/Equipaments/List/index.tsx
+++ src/pages/Equipaments/List/index.tsx
@@ -1,38 +1,53 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
-import { Link, useHistory } from 'react-router-dom';
-import { FiPlus } from 'react-icons/fi';
+import { useHistory } from 'react-router-dom';
+import { useQueryParam, StringParam } from 'use-query-params';
 import api from '../../../services/api';
 import Pagination from '../../../components/Pagination';
 import IEquipament from '../../../interfaces/IEquipament';
-import { Container, HeaderContent, Table, Row } from './styles';
+import { Container, Table, Row } from './styles';
+import Header from '../../../components/Header';
+
+interface ISearchFormData {
+  term: string;
+}
 
 const List: React.FC = () => {
   const [equipaments, setEquipaments] = useState<IEquipament[]>([]);
   const [total, setTotal] = useState<number>(0);
-  const [limit, setLimit] = useState(5);
+  const [limit] = useState(10);
   const [currentPage, setCurrentPage] = useState<number>(1);
+  const [queryName, setQueryName] = useState<string | undefined>();
   const history = useHistory();
 
   useEffect(() => {
     async function loadEquipaments() {
-      const params = { page: currentPage, limit };
+      const params = { page: currentPage, limit, queryName };
       const response = await api.get('/equipaments', { params });
       setEquipaments(response.data);
       setTotal(Number(response.headers['x-total-count']));
     }
     loadEquipaments();
-  }, [currentPage, limit]);
+  }, [currentPage, limit, queryName]);
+
+  const handleSearchSubmit = useCallback(
+    ({ term }: ISearchFormData) => {
+      console.log(term);
+      setCurrentPage(1);
+      setQueryName(term || undefined);
+    },
+    [setQueryName, setCurrentPage],
+  );
 
   return (
     <Container>
-      <HeaderContent>
-        <Link to="/equipaments/new">
-          <FiPlus size={20} />
-          Cadastrar
-        </Link>
-        <h1>Lista de equipamentos</h1>
-      </HeaderContent>
+      <Header
+        initialName={queryName}
+        onSubmit={handleSearchSubmit}
+        createPage="/equipaments/new"
+        title="Equipamentos"
+        placeholder="Digite um termo de pesquisa"
+      />
       <Table>
         <thead>
           <tr>
--- src/pages/Equipaments/List/styles.ts
+++ src/pages/Equipaments/List/styles.ts
@@ -1,35 +1,7 @@
 import styled from 'styled-components';
 import { shade } from 'polished';
 
-export const Container = styled.div`
-  h1 {
-    margin-bottom: 2rem;
-  }
-`;
-
-export const HeaderContent = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 2rem;
-  a {
-    display: flex;
-    font-weight: 500;
-    align-items: center;
-    justify-content: center;
-    text-decoration: none;
-    color: ${({ theme }) => theme.colors.white};
-    text-transform: uppercase;
-    height: 5rem;
-    padding: 0 1rem;
-    background: ${({ theme }) => theme.colors.primary};
-    border-radius: ${({ theme }) => theme.radii.default};
-    box-shadow: ${({ theme }) => theme.shadows.default};
-    &:hover {
-      background: ${({ theme }) => shade(0.2, theme.colors.primary)};
-    }
-  }
-`;
+export const Container = styled.div``;
 
 export const Table = styled.table`
   width: 100%;

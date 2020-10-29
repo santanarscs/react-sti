7da72640b3edbbe904f7065fde06ebfa187f7bc5
Raphael Santana <raphaelstn@gmail.com>
2020-10-29 08:55:57 -0300

    feat(orders): add order detaill

--- src/pages/Orders/Detail/index.tsx
+++ src/pages/Orders/Detail/index.tsx
@@ -1,39 +1,140 @@
-import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useState, useEffect, useCallback } from 'react';
+import { parseISO, formatDistance } from 'date-fns';
+import pt from 'date-fns/locale/pt';
+import { useParams, useHistory } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import api from '../../../services/api';
+import Delete from './Delete';
+import { useModal } from '../../../hooks/modal';
+import Solve from './Solve';
+import parseDate from '../../../utils/parseDate';
+import { Container, HeaderContent, SolvedButton, DeleteButton } from './styles';
 
-import { Container } from './styles';
+interface IType {
+  name: string;
+  color: string;
+}
+
+interface IUser {
+  name: string;
+}
+
+interface IOrder {
+  id: string;
+  description: string;
+  created_at: string;
+  user: string;
+  type: IType;
+  solver: IUser;
+  status: string;
+  resolution_date: string;
+  resolution: string;
+}
 
 const Detail: React.FC = () => {
   const { id } = useParams();
-  const [order, setOrder] = useState({} as any);
+  const [order, setOrder] = useState<IOrder>();
+  const { openModal } = useModal();
+  const history = useHistory();
+
   useEffect(() => {
-    api.get(`/orders/${id}`).then((response) => setOrder(response.data));
+    api.get(`/orders/${id}`).then((response) =>
+      setOrder({
+        ...response.data,
+        created_at: formatDistance(
+          parseISO(response.data.created_at),
+          new Date(),
+          {
+            addSuffix: true,
+            locale: pt,
+          },
+        ),
+      }),
+    );
   }, [id]);
+
+  const handleRedirect = useCallback(() => {
+    history.push('/orders');
+  }, [history]);
+
+  const handleRemoveItem = useCallback(() => {
+    openModal({
+      title: 'Remover serviço',
+      container: () => Delete({ id, handleRedirect }),
+    });
+  }, [id, openModal, handleRedirect]);
+
+  const handleOpenSolveModal = useCallback(() => {
+    openModal({
+      title: 'Resolver serviço',
+      container: () => Solve({ id, handleRedirect }),
+    });
+  }, [openModal, id, handleRedirect]);
+
+  if (!order) {
+    return (
+      <Container>
+        <h1>Carregando</h1>
+      </Container>
+    );
+  }
   return (
     <Container>
-      <h1>Detalhes</h1>
+      <HeaderContent>
+        <h1>Detalhes</h1>
+        <div>
+          <Link to="/orders">Voltar</Link>
+          {order.status !== 'FECHADO' && (
+            <>
+              <Link to={`/orders/edit/${order.id}`}>Editar</Link>
+              <SolvedButton type="button" onClick={handleOpenSolveModal}>
+                Resolver
+              </SolvedButton>
+              <DeleteButton type="button" onClick={handleRemoveItem}>
+                Remover
+              </DeleteButton>
+            </>
+          )}
+        </div>
+      </HeaderContent>
       <ul>
         <li>
           <strong>Data da criação: </strong>
-          <span>{order.data_criacao}</span>
+          <span>{order.created_at}</span>
         </li>
         <li>
-          <strong>Descrição: </strong>
-          <span>{order.descricao}</span>
-        </li>
-        <li>
-          <strong>Status: </strong>
-          <span>{order.local}</span>
+          <strong>Tipo: </strong>
+          <span>{order.type.name}</span>
         </li>
         <li>
           <strong>Usuário: </strong>
-          <span>{order.usuario}</span>
+          <span>{order.user}</span>
         </li>
         <li>
-          <strong>Solucionador: </strong>
-          <span>{order.solucionador}</span>
+          <strong>Status: </strong>
+          <span>{order.status}</span>
         </li>
+
+        <li>
+          <strong>Descrição: </strong>
+          <span>{order.description}</span>
+        </li>
+        {order.status === 'FECHADO' && (
+          <>
+            <li>
+              <strong>Solucionador: </strong>
+              <span>{order.solver.name}</span>
+            </li>
+            <li>
+              <strong>Data da solução: </strong>
+              <span>{parseDate(order.resolution_date)}</span>
+            </li>
+            <li>
+              <strong>Solução: </strong>
+              <span>{order.resolution}</span>
+            </li>
+          </>
+        )}
       </ul>
     </Container>
   );
--- src/pages/Orders/Detail/styles.ts
+++ src/pages/Orders/Detail/styles.ts
@@ -1,16 +1,15 @@
 import styled from 'styled-components';
+import { shade } from 'polished';
 
 export const Container = styled.div`
-  h1 {
-    margin-bottom: 6rem;
+  > div {
+    display: flex;
   }
   ul {
-    margin-right: 3rem;
     background: #323846;
     padding: 2rem;
     border-radius: ${({ theme }) => theme.radii.default};
     box-shadow: ${({ theme }) => theme.shadows.default};
-    border-left: 1rem solid ${({ theme }) => theme.colors.secondary};
 
     li {
       margin-bottom: 2rem;
@@ -18,5 +17,62 @@ export const Container = styled.div`
         margin-right: 0.5rem;
       }
     }
+  }
+`;
+export const HeaderContent = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 6rem;
+  margin-bottom: 3.2rem;
+  h1 {
+    font-weight: normal;
+  }
+
+  > div {
+    display: flex;
+
+    a {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      text-decoration: none;
+      text-transform: uppercase;
+      margin-left: 1rem;
+      background: ${({ theme }) => theme.colors.grey};
+      color: ${({ theme }) => theme.colors.white};
+      padding: 1rem 1.6rem;
+      border-radius: ${({ theme }) => theme.radii.default};
+      box-shadow: ${({ theme }) => theme.shadows.default};
+      &:hover {
+        background: ${({ theme }) => shade(0.2, theme.colors.grey)};
+      }
+    }
+  }
+`;
+
+export const SolvedButton = styled.button`
+  text-transform: uppercase;
+  margin-left: 1rem;
+  background: ${({ theme }) => theme.colors.primary};
+  color: ${({ theme }) => theme.colors.white};
+  padding: 1rem 1.6rem;
+  border-radius: ${({ theme }) => theme.radii.default};
+  box-shadow: ${({ theme }) => theme.shadows.default};
+  &:hover {
+    background: ${({ theme }) => shade(0.2, theme.colors.primary)};
+  }
+`;
+
+export const DeleteButton = styled.button`
+  text-transform: uppercase;
+  margin-left: 1rem;
+  background: ${({ theme }) => theme.colors.error};
+  color: ${({ theme }) => theme.colors.white};
+  padding: 1rem 1.6rem;
+  border-radius: ${({ theme }) => theme.radii.default};
+  box-shadow: ${({ theme }) => theme.shadows.default};
+  &:hover {
+    background: ${({ theme }) => shade(0.2, theme.colors.error)};
   }
 `;
